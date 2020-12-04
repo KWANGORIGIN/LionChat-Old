96bb3be0c264df9881e2446142f1c7b285cdb4f6
William <willhemminger30@gmail.com>
2020-12-03 22:52:31 -0500

    Added flags to entities, changed UserOSEntity to OperatingSystemEntity

---
+++ entity/entities/OperatingSystemEntity.java
@@ -0,0 +1,25 @@
+package psu.lionchat.entity.entities;
+
+import psu.lionchat.entity.Entity;
+
+public class OperatingSystemEntity extends Entity {
+	protected String userOs;
+
+	public OperatingSystemEntity() {
+		this.entityName = "User OS";
+	}
+
+	@Override
+	public String getEntityInformation() {
+		return userOs;
+	}
+
+	@Override
+	public void setEntityInformation(String info) {
+		this.userOs = info;
+		this.hasInfo = true;
+
+	}
+
+
+}
