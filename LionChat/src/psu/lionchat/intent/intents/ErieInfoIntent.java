83d7891401d4d15da0013b391f485f4281284329
Kevin <wangatang53@gmail.com>
2020-12-07 22:59:42 -0500

    Added ErieInfoIntent class

---
+++ LionChat/src/psu/lionchat/intent/intents/ErieInfoIntent.java
@@ -0,0 +1,10 @@
+package psu.lionchat.intent.intents;
+
+import psu.lionchat.intent.Intent;
+
+public class ErieInfoIntent extends Intent {
+	public ErieInfoIntent() {
+		super();
+		this.intentName = "Erie Info";
+	}
+}
