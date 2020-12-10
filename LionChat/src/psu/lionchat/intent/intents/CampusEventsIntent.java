2f7a524cbe4ec52fe35fafde8fbf05aed14c571f
William <willhemminger30@gmail.com>
2020-12-10 12:24:47 -0500

    Location Entity Removed, CampusEventsIntent modified

--- LionChat/src/psu/lionchat/intent/intents/CampusEventsIntent.java
+++ LionChat/src/psu/lionchat/intent/intents/CampusEventsIntent.java
@@ -1,7 +1,6 @@
 package psu.lionchat.intent.intents;
 
 import psu.lionchat.entity.entities.DateTimeEntity;
-import psu.lionchat.entity.entities.LocationEntity;
 import psu.lionchat.intent.Intent;
 
 public class CampusEventsIntent extends Intent {
