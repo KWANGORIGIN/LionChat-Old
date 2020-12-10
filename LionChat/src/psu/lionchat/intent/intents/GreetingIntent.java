756cd4e2163fd269415501bad9702faa3c68ab5e
William <willhemminger30@gmail.com>
2020-12-10 03:19:00 -0500

    added greeting intent

---
+++ LionChat/src/psu/lionchat/intent/intents/GreetingIntent.java
@@ -0,0 +1,16 @@
+package psu.lionchat.intent.intents;
+
+import psu.lionchat.intent.Intent;
+
+public class GreetingIntent extends Intent {
+    public GreetingIntent() {
+        super();
+        this.intentName = "Greeting Intent";
+    }
+
+    @Override
+    public String toString(){
+        return "GreetingIntent";
+    }
+
+}
